5527ff3ad3eb9663b80e6435f6f47bb0c67542c7
titko <toni.itkonen@icloud.com>
2019-01-29 22:10:17 +0200

    Initial commit

---
+++ Space shooter R1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour 
+	{
+
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	private Rigidbody rb; //lisäsin tämän
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
+	private AudioSource audioSource; //lisäsin tämän 14.11. audio kohdassa
+
+
+	void Update ()
+	{
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+			//GameObject clone = 
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation); // as GameObject;
+			//audio.Play ();
+
+			audioSource = GetComponent <AudioSource> (); //lisäsin tämän 14.11. audio kohdassa
+			audioSource.Play(0); //lisäsin tämän 14.11. audio kohdassa
+			Debug.Log ("started"); //lisäsin tämän 14.11. audio kohdassa
+
+		}
+	}
+
+
+
+	void Start() //lisäsin tämän myös
+	{
+		rb = GetComponent<Rigidbody> ();
+	
+	}
+
+		void FixedUpdate ()
+		{
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed; //muutin rigidbody > rb
+
+		rb.position = new Vector3 
+		(
+			Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+
+		}
+
+	}
---
+++ Space shooter R1/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour 
+{
+	public float tumble;
+
+	private Rigidbody rb; //lisäsin tämän
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+
+	}
+
+
+
+}
